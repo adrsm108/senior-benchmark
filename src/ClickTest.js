76ce6e25d99c501c38dc4b71999da43d1d802ed8
Adam Smith <adrsm108@gmail.com>
2020-08-01 02:33:12 -0400

    Implement ClickTest component

--- src/ClickTest.js
+++ src/ClickTest.js
@@ -1,62 +1,217 @@
-import React, {Component, useEffect, useRef, useState} from 'react';
+import React, {Component} from 'react';
 import './ClickTest.less';
+import {classConcat, dist2d, formatMilliseconds, mapLength, takeValues} from './utils';
+import {Divider, Space, Table, Typography} from 'antd';
+import _ from 'lodash-es';
 
-const targetPath = new Path2D('M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0 z');
+const {Text, Title} = Typography;
 
-function drawTarget(ctx, {x, y}) {
-  console.log('drawing targ');
-  ctx.fillStyle('red');
-  ctx.translate(x, y);
-  ctx.fill(targetPath);
-  ctx.restore();
+function Target(props) {
+  const {r, pos, rings, cr, ...rest} = props;
+  const s = r - cr;
+  return (
+    <svg x={pos[0]} y={pos[1]} {...rest} className="Target">
+      {mapLength(rings + 1, (n) => (
+        <circle
+          key={n}
+          r={(s * (rings - n)) / rings + cr}
+          className={classConcat(
+            'target-ring',
+            n === 0 && 'target-body', // first circle
+            n === rings && 'target-center' // last circle
+          )}
+        />
+      ))}
+    </svg>
+  );
 }
 
-function useCanvas(width, height) {
-  const canvasRef = useRef(null);
-  const [pts, setPts] = useState([]);
+Target.defaultProps = {
+  pos: [0, 0], // x, y location
+  r: 50, // target radius
+  rings: 3, // number of rings
+  cr: 4, // center radius
+};
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, width, height);
-
-    pts.forEach((pt) => {
-      drawTarget(ctx, pt);
-    });
-  });
-
-  return [pts, setPts, canvasRef, width, height];
+function StartButton(props) {
+  return (
+    <svg x="50%" y="50%" className="StartButton">
+      <circle {...props} className="button-body" />
+      <text className="button-text">{props.children}</text>
+    </svg>
+  );
 }
+
+const RESULTS_COLUMNS = [
+  {
+    title: 'Position',
+    children: [
+      {title: 'Spawn', dataIndex: 'pos', render: ([x, y]) => `[${x}, ${y}]`},
+      {title: 'Click', dataIndex: 'clickPos', render: ([x, y]) => `[${x}, ${y}]`},
+      {
+        title: 'Distance',
+        key: 'elapsed',
+        render: (text, row) => dist2d(row.pos, row.clickPos).toFixed(3),
+      },
+    ],
+  },
+  {
+    title: 'Time',
+    children: [
+      {title: 'Spawn', dataIndex: 'spawnTime', render: (n) => n.toFixed(3)},
+      {title: 'Click', dataIndex: 'clickTime', render: (n) => n.toFixed(3)},
+      {
+        title: 'Elapsed',
+        key: 'elapsed',
+        render: (text, row) => formatMilliseconds(row.clickTime - row.spawnTime),
+      },
+    ],
+  },
+];
 
 class ClickTest extends Component {
   constructor(props) {
     super(props);
-    const [pts, setPts, canvasRef, width, height] = useCanvas(props.width, props.height);
-    this.canvas = {
-      pts: pts,
-      setPts: setPts,
-      ref: canvasRef,
-      width: width,
-      height: height,
+    this.testLog = [];
+    this.testArea = React.createRef();
+    this.state = {
+      testActive: false,
+      target: null,
+      remainingTargets: null,
+      testTimeout: null,
+      results: null,
     };
   }
 
-  handleCanvasClick = (event) => {
-    this.canvas.setPts([...this.canvas.pts, {x: event.clientX, y: event.clientY}]);
+  componentDidMount() {
+    this.setState({
+      target: this.createTarget(true),
+    });
+  }
+
+  componentWillUnmount() {
+    if (!this.state.testTimeout) return;
+    this.setState((state) => {
+      clearTimeout(state.testTimeout);
+      return {testTimeout: null};
+    });
+  }
+
+  createTarget = () => {
+    return {
+      pos: this.testArea.current
+        ? takeValues(
+            this.testArea.current,
+            ['clientWidth', 'clientHeight'],
+            (x) => Math.floor(Math.random() * x) // integer coordinates
+          )
+        : [0, 0],
+      spawnTime: performance.now(),
+    };
+  };
+
+  handleTargetClick = ({nativeEvent: event}) => {
+    const now = performance.now();
+    if (this.state.target.clickTime) {
+      console.log('duplicate click on target');
+      return;
+    }
+    this.testLog.push(
+      Object.assign(this.state.target, {
+        clickPos: [event.offsetX, event.offsetY],
+        clickTime: now,
+      })
+    );
+    this.setState((state) => {
+      return state.remainingTargets > 0
+        ? {
+            remainingTargets: state.remainingTargets - 1,
+            testActive: true,
+            target: this.createTarget(),
+          }
+        : {
+            testActive: false,
+            remainingTargets: null,
+            target: null,
+            results: this.testLog,
+          };
+    });
+  };
+
+  handleTestStart = () => {
+    if (this.testActive) return;
+    console.log('starting test.');
+    this.testLog = [];
+    this.setState({
+      testActive: true,
+      remainingTargets: this.props.testLength - 1,
+      target: this.createTarget(),
+      results: null,
+    });
   };
 
   render() {
     return (
-      <div className="click-test-main">
-        <canvas
-          width={this.props.width}
-          height={this.props.height}
-          ref={this.canvas.ref}
-          onClick={this.handleCanvasClick}
-        />
+      <div className="ClickTest" style={this.props.style}>
+        <Space direction="vertical">
+          <Title className="main-title">Click Test</Title>
+          <div
+            className="test-area-container"
+            style={{
+              padding: this.props.targetRadius,
+              ..._.pick(this.props, ['minWidth', 'maxWidth', 'minHeight', 'maxHeight']),
+            }}
+          >
+            <svg
+              className="test-area"
+              ref={this.testArea}
+              height={this.props.height - 2 * this.props.targetRadius}
+            >
+              {this.state.testActive ? (
+                <Target
+                  onClick={this.handleTargetClick}
+                  pos={this.state.target.pos}
+                  r={this.props.targetRadius}
+                />
+              ) : (
+                <StartButton onClick={this.handleTestStart} r={this.props.targetRadius}>
+                  Start!
+                </StartButton>
+              )}
+            </svg>
+          </div>
+          <Space>
+            {this.state.testActive ? (
+              <Text key="remaining">Remaining: {this.state.remainingTargets}</Text>
+            ) : (
+              <Text key="status">Test Inactive</Text>
+            )}
+          </Space>
+          {this.state.results && [
+            <Divider key="divider">
+              <Title>Results</Title>
+            </Divider>,
+            <Table
+              key="table"
+              bordered
+              pagination={false}
+              columns={RESULTS_COLUMNS}
+              dataSource={this.state.results}
+              rowKey="spawnTime"
+            />,
+          ]}
+        </Space>
       </div>
     );
   }
 }
+
+ClickTest.defaultProps = {
+  minWidth: 450,
+  maxWidth: 1000,
+  height: 500,
+  targetRadius: 50,
+  testLength: 5,
+};
 
 export default ClickTest;
---
+++ src/utils.js
@@ -0,0 +1,130 @@
+/*
+ * utils.js
+ *
+ * Some general helper functions, mostly functional programming constructs that js doesn't have standard versions of.
+ * Feel free to add!
+ *
+ * */
+
+//------------ PRIVATE FUNCTIONS  ------------//
+
+// Identity function (nice version so webstorm doesn't complain about arity)
+function identity(x, ..._) {
+  return x;
+}
+
+//------------ EXPORTED FUNCTIONS  ------------//
+
+/* Returns space-separated string concatenation of truthy arguments.
+
+Useful for conditionally assigning classes.
+classConcat(cond1 && class1, cond2 && class2, ...) ~~> string of classes whose associated conditions are true.
+*/
+export function classConcat(...classes) {
+  return classes.reduce((classList, item) => (item ? `${classList} ${item}` : classList), '');
+}
+
+/* Group items in a collection using a function.
+groupBy(coll, f) returns a new Map
+{ x1 => [ e in coll such that f(e) === x1 ],
+  x2 => [ e in coll such that f(e) === x2 ], ... }
+*/
+export function groupBy(coll, f) {
+  const map = new Map();
+  for (const el of coll) {
+    const key = f(el);
+    if (map.has(key)) {
+      map.get(key).push(el);
+    } else {
+      map.set(key, [el]);
+    }
+  }
+  return map;
+}
+
+/* Count the number of items in a collection.
+tally(coll) returns a new Map associating distinct elements of coll with the # of times they appear.
+tally(coll, f) returns a new Map
+{ x1 => (# of e in coll such that f(e) === x1),
+  x2 => (# of e in coll such that f(e) === x2), ... }
+* */
+export function tally(coll, f = identity) {
+  const map = new Map();
+  for (const el of coll) {
+    const key = f(el);
+    map.set(key, (map.get(key) || 0) + 1);
+  }
+  return map;
+}
+
+// 2D Euclidean distance
+export function dist2d([x0, y0], [x1, y1]) {
+  return Math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2);
+}
+
+// Returns a string representation of the given number of milliseconds in hh:mm:ss:fff format,
+// with hh and mm omitted if they are 0.
+export function formatMilliseconds(ms) {
+  const sec = Math.floor(ms / 1000);
+  const min = Math.floor(sec / 60);
+  const hr = Math.floor(min / 60);
+  return (
+    (hr ? hr.toString().padStart(2, '0') + ':' : '') +
+    (min ? min.toString().padStart(2, '0') + ':' : '') +
+    `${sec.toString().padStart(2, '0')}:${Math.floor(ms % 1000)
+      .toString()
+      .padStart(3, '0')}`
+  );
+}
+
+// Returns execution time for f(...args) along with its result
+export function timing(f, ...args) {
+  const t0 = performance.now();
+  const res = f(...args);
+  const t1 = performance.now();
+
+  return {time: t1 - t0, result: res};
+}
+
+// Runs f(...args) n times, returning execution time statistics and last evaluation result.
+export function repeatedTiming(n, f, ...args) {
+  n = Math.floor(n);
+  if (n < 1) throw new TypeError('n should be greater than 1.');
+  if (n === 1) return timing(f, ...args);
+  const times = [];
+  let t0, t1, result;
+  let i = n;
+  while (i--) {
+    t0 = performance.now();
+    result = f(...args);
+    t1 = performance.now();
+    times.push(t1 - t0);
+  }
+
+  const avgTime = times.reduce((a, b) => a + b, 0) / n;
+  return {
+    avgTime,
+    trials: n,
+    minTime: Math.min(...times),
+    maxTime: Math.max(...times),
+    stdDev: Math.sqrt(times.reduce((s, t) => s + (t - avgTime) ** 2, 0) / (n - 1)),
+    result,
+  };
+}
+
+/* Maps a function over a range of integers without generating an intermediate array
+ mapLength(n, f) ~~> [ f(0), f(1), ..., f(floor(n-1)) ] if n >= 0
+                     [ f(0), f(-1), ..., f(ceil(n+1)) ] if n < 0
+ Saves memory over range(n).map(f)
+*/
+export function mapLength(n, f) {
+  return Array.from({length: Math.abs(n)}, n >= 0 ? (_, i) => f(i) : (_, i) => f(-i));
+}
+
+/* Gets the specified properties of an object as an array, with optional transformation.
+ takeValues(obj, [k1, k2, ...]) ~~> [ obj[k1], obj[k2], ... ]
+ takeValues(obj, [k1, k2, ...], f) ~~> [ f(obj[k1], k1), f(obj[k2], k2), ... ]
+*/
+export function takeValues(obj, keys, f = identity) {
+  return keys.map((k) => f(obj[k], k));
+}
